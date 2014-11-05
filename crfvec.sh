745898770ae38f6e5a4e60a7436ea0d020da69e2
xlhdh <hu_yizhou@hotmail.com>
2014-11-05 01:23:18 -0600

    test script

--- crfvec.sh
+++ crfvec.sh
@@ -1,3 +1,8 @@
-python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwsg500.txt' 2 1
-python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwglove500.txt' 2 1
-python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwcbow500.txt' 2 1
+python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwsg50.txt' 2 1
+python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwglove50.txt' 2 1
+python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwcbow50.txt' 2 1
+
+python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwsg300.txt' 2 1
+python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwglove300.txt' 2 1
+python runcrf.py 'data/sjw/*' 10000 'data/vector/sjwcbow300.txt' 2 1
+
